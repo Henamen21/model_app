e29ff2115cfdeda7bfd6e8109ede25e9bf235f15
Henamen21 <heneyr24@gmail.com>
2023-08-14 09:01:11 +0300

    prediction successfull

--- gradio_project/app.py
+++ gradio_project/app.py
@@ -1,19 +1,43 @@
-import streamlit as st
-import pandas as pd
+import gradio as gr
+import pickle
 import os
-import pickle 
-import numpy as np
+import sklearn
+import pandas as pd
+ 
+title = "Vodafone Telecommunication : Customer Churn Prediction" 
+description = "Vodafone Telecommunication is a leading global telecommunications company.\
+                One of the challenges that Vodafone faces is customer churn, \
+                which is the loss of customers to other service providers. \
+                Customer churn can have a negative impact on the revenue \
+                and profitability of the company."
 
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler 
+# Define the input components and their labels
+inputs = [              
 
-# Company image
-st.image("https://upload.wikimedia.org/wikipedia/commons/0/0f/Corporaci%C3%B3n_Favorita_Logo.png?20161217003545")
+    gr.inputs.Dropdown(["Male", "Female"], label="gender"),
+    gr.inputs.Dropdown(["Yes", "No"], label="SeniorCitizen"),
+    gr.inputs.Dropdown(["Yes", "No"], label="Partner"),   
+    gr.inputs.Dropdown(["Yes", "No"], label="Dependents"),
+    gr.inputs.Dropdown(["Yes", "No"],label="PhoneService"),
+    gr.inputs.Dropdown(["No phone service", "Yes", "No"], label="MultipleLines"),  
+    gr.inputs.Dropdown(["DSL", "Fiber optic", "No"], label="InternetService"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="OnlineSecurity"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="OnlineBackup"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="DeviceProtection"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="TechSupport"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="StreamingTV"),
+    gr.inputs.Dropdown(["No internet service", "Yes", "No"], label="StreamingMovies"),
+    gr.inputs.Radio(["Month-to-month", "One year", "Two year"], label="Contract"),
+    gr.inputs.Dropdown(["Yes", "No"], label="PaperlessBilling"),
+    gr.inputs.Radio(["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"], label="PaymentMethod"),
+    gr.inputs.Number(default=0,  label="tenure"),
+    gr.inputs.Number(default=0,  label="MonthlyCharges"),
+    gr.inputs.Number(default=0,  label="TotalCharges")
+ 
+]
 
-st.write("""# Corporation Favorita Grocery """)
-
-# Write a subheader
-st.subheader("Enter your records")
+# Define the output component and its label
+output = gr.outputs.Label(label="Churn Prediction") 
 
 # Load ml components from file
 cwd = os.getcwd()
@@ -21,40 +45,60 @@ relative_path = "gradio_project\\pipeline.pkl"
 
 absolute_path = os.path.join(cwd, relative_path)
 print(absolute_path)
+print(inputs)
 
 # Reading Machine learning component file
 with open(absolute_path, "rb") as f:
     ml_components = pickle.load(f)
 
-pip = ml_components
+print(ml_components)
+      
+# Define a conversion prediction function
+def convert_to_df(gender, SeniorCitizen, Partner , Dependents , 
+                PhoneService , MultipleLines , InternetService , OnlineSecurity , 
+                OnlineBackup, DeviceProtection , TechSupport , StreamingTV, 
+                StreamingMovies , Contract , PaperlessBilling , PaymentMethod ,  
+                tenure , MonthlyCharges , TotalCharges):
+    
+    
+    # Create a list of lists from the input arguments
+    data = [[gender, SeniorCitizen, Partner , Dependents , 
+                PhoneService , MultipleLines , InternetService , OnlineSecurity , 
+                OnlineBackup, DeviceProtection , TechSupport , StreamingTV, 
+                StreamingMovies , Contract , PaperlessBilling , PaymentMethod ,  
+                tenure , MonthlyCharges , TotalCharges]]
+    
+    # Create a dataframe from the data  
+    df = pd.DataFrame(data, columns=["gender", "SeniorCitizen", "Partner" , "Dependents" , 
+                "PhoneService" , "MultipleLines" , "InternetService" , "OnlineSecurity" , 
+                "OnlineBackup", "DeviceProtection" , "TechSupport" , "StreamingTV", 
+                "StreamingMovies" , "Contract" , "PaperlessBilling" , "PaymentMethod" ,  
+                "tenure" , "MonthlyCharges" , "TotalCharges"])
+    
+    print("check")
+    print(df)
+    print("predict") 
+    
+    pipeline = ml_components["pipline"]
+    predicted = pipeline.predict(df)
 
-print(pip)
+    # Convert the predicted class into a dictionary
+    classes = ["Yes", "No"]
+    output = {classes[classes.index(predicted[0])]: 1.0}
+    
+    # Return the dictionary
+    print(output)
+    return output
 
-inputs = [
-    ["gender" , "SeniorCitizen", "Partner",
-    'Dependents', 'PhoneService', 'MultipleLines',
-    'InternetService', 'OnlineSecurity',
-    'OnlineBackup', 'DeviceProtection',
-    'TechSupport', 'StreamingTV',
-    'StreamingMovies', 'Contract',
-    'PaperlessBilling', 'PaymentMethod',
-    'tenure', 'MonthlyCharges', 'TotalCharges'
-    ], 
+# Create and launch the interface
+iface = gr.Interface(
+    fn=convert_to_df,
+    title = title,
+    description=description, 
+    inputs=inputs, 
+    outputs="label",
+    theme=gr.themes.GoogleFont(name="Roboto")
+    )
+iface.launch(share = "True")
 
-    [ "Male" , "No", "No",
-    "No" , "Yes", "No",
-    "Fiber optic" , "No",
-    "No" , "No",
-    "No" , "No",
-    "Yes" , "Month-to-month",
-    "Yes", "Electronic check",
-    12 , 23 ,32
-    ], 
-
-    ]
-
-df = pd.DataFrame(inputs[1:], columns=inputs[0])
-st.write(df)
-pred = pip.predict(df)
-
-print(pred)
+    
